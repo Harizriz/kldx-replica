833746d0aef7dea3cdbe9806acc0861e6983d36a
hariz <hariz.hafiz@malaysiakini.com>
2024-12-04 19:18:46 +0800

    feat: Added video section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,17 @@
 <script lang="ts">
+	let words = [
+		'Private Equity',
+		'Private Debt',
+		'Private Funds',
+		'ESG and Sustainability',
+		'Alternatives'
+	];
+	let currentIndex = 0;
+
+	// Change the word every 2 seconds (1.5 seconds for staying + 0.5 seconds for transition)
+	setInterval(() => {
+		currentIndex = (currentIndex + 1) % words.length;
+	}, 2000);
 </script>
 
 <svelte:head>
@@ -6,7 +19,7 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section>
+<section class="relative w-full">
 	<!-- <h1>
 		<span class="welcome">
 			<picture>
@@ -23,34 +36,56 @@
 	</h2>
 
 	<Counter /> -->
+	<video
+		autoplay
+		muted
+		loop
+		class="w-full"
+		src="https://kldx.com/wp-content/themes/kapital-dx/videos/newvideo2.mp4"
+	>
+		<track kind="captions" />
+	</video>
+	<div
+		class="absolute left-1/2 top-1/2 flex w-full -translate-x-1/2 -translate-y-1/2 transform flex-col items-center justify-center"
+	>
+		<h1 class="leading-tight text-white">Expand your<br />Investment Universe</h1>
+		<p class="mt-1 text-white">
+			A secure, trusted platform that empowers your private market investments.
+		</p>
+		<p class="mt-4 text-white">Tap into the power of investing directly</p>
+		<div class="mt-4 flex items-center justify-center">
+			{#each words as word, index (word)}
+				<span
+					class={`absolute font-semibold text-white opacity-0 ${index === currentIndex ? 'word-enter visible' : 'hidden'}`}
+				>
+					{word}
+				</span>
+			{/each}
+		</div>
+	</div>
 </section>
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
+	.word-enter {
+		animation: scrollUp 2s ease-in-out forwards;
 	}
 
-	/* h1 {
-		width: 100%;
+	@keyframes scrollUp {
+		0% {
+			opacity: 0;
+			transform: translateY(50%);
+		}
+		25% {
+			opacity: 1;
+			transform: translateY(0);
+		}
+		75% {
+			opacity: 1;
+			transform: translateY(0);
+		}
+		100% {
+			opacity: 0;
+			transform: translateY(-50%);
+		}
 	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	} */
 </style>
